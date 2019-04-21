08604812b549a3e7a551da29d49085b82ffd415b
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-04-21 18:50:59 +0200

    disp simplify eq

--- matlab/doublecartpole_lagrange.m
+++ matlab/doublecartpole_lagrange.m
@@ -12,8 +12,8 @@ qddot = [qddot_0; qddot_1; qddot_2];  % time derivative of qdot
 
 % kinematics:
 p_c = [q_0; 0];
-p_1 = p_c + r_1/2 * [cos(q_1); sin(q_1)];
-p_2 = p_c + r_1 * [cos(q_1); sin(q_1)] + r_2/2 * [cos(q_1+q_2); sin(q_1+q_2)];
+p_1 = p_c + r_1 * [cos(q_1); sin(q_1)];
+p_2 = p_c + r_1 * [cos(q_1); sin(q_1)] + r_2 * [cos(q_1+q_2); sin(q_1+q_2)];
 
 v_c = jacobian(p_c, q_0) * qdot_0;
 v_1 = jacobian(p_1, [q_0; q_1]) * [qdot_0; qdot_1];
@@ -45,8 +45,16 @@ d_inner_by_dt = jacobian(partial_L_by_partial_qdot, q) * qdot + jacobian(partial
 % Euler-Lagrange equation
 lagrange_eq = partial_L_by_partial_q - d_inner_by_dt == [f;0;0];
 
-% solve the lagrange equation for qddot (takes a few seconds)
-r = solve(lagrange_eq, qddot);
+% solve the lagrange equation for qddot and simplify (takes a while)
+r = solve(simplify(lagrange_eq), qddot);
+
+qddot_0 = simplify(r.qddot_0);
+qddot_1 = simplify(r.qddot_1);
+qddot_2 = simplify(r.qddot_2);
+
+disp('qddot_0 = '); disp(r.qddot_0);
+disp('qddot_1 = '); disp(r.qddot_1);
+disp('qddot_2 = '); disp(r.qddot_2);
 
 % generate Matlab function
 matlabFunction([qdot_0; qdot_1; qdot_2; r.qddot_0; r.qddot_1; r.qddot_2], 'file', 'doublecartpole_dynamics_generated', 'Vars', [q;qdot;f;p])
