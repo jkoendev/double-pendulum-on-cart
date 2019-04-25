581456e9fe4f34fcbf1cf3f50e8fd6d4da466f8a
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-04-25 12:46:09 +0200

    deply script zip

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,20 @@
-zip -r dpc_matlab.zip matlab README.md
-zip -r dpc_python.zip python README.md
+mkdir tmp
+cp README.md tmp
+cp matlab/* tmp
+cp -r matlab/simplified/ tmp
+cd tmp
+zip -r dpc_matlab.zip .
+cd ..
+cp tmp/dpc_matlab.zip .
 
+rm -rf tmp
 
+mkdir tmp
+cp README.md tmp
+cp python/* tmp
+cp -r python/simplified/ tmp
+cd tmp
+zip -r dpc_python.zip .
+cd ..
+cp tmp/dpc_python.zip .
+rm -rf tmp
